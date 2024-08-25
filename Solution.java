f18d276594276c46fd20365934c8b34c8c167204
Akashkr28 <email.akash2010@gmail.com>
2024-08-25 10:39:48 +0530

    friendPairProblem

---
+++ Solution.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+    public static long mod = 1000000007;
+    public static long[] dp;
+    public static long f(int i) {
+        if(i == 0 || i == 1 || i == 2){
+            return i;
+        }
+
+        if(dp[i] != -1) return dp[i] ;
+
+        long singleWays = f(i - 1)%mod;
+        long pairWays = ((i-1)%mod * f(i-2)%mod)%mod;
+        return dp[i] = ((singleWays%mod) + (pairWays%mod))%mod;
+    }
+    public static int numberOfWays(int n) {
+        dp = new long[10005];
+        Arrays.fill(dp, -1);
+        return (int)f(n);
+    }
+}
+
+// (a + b)%c => [a%c + b%c]%c
+// (a * b)%c => [a%c * b%c]%c
+// (a - b)%c => [a%c - b%c + c]%c
+
