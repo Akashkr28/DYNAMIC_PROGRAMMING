d4206ba7e04047226bc5ee4c29696520d0ed4e6c
Akashkr28 <email.akash2010@gmail.com>
2024-08-26 22:25:50 +0530

    soved three DP problems

---
+++ perfectSquares.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public static int[] c; // global coins array
+    public static int[] dp;
+
+    public int f(int amount){
+        if (amount < 0) return Integer.MAX_VALUE; // if amt is less than 0 then it's not possible
+        if (amount == 0) return 0; // if amt is 0, give nothing
+        if (dp[amount] != -1) return dp[amount];
+        int ans = Integer.MAX_VALUE;
+        int n = c.length;
+        for(int i = 0; i < n; i++) {
+            if (amount >= c[i]) {
+                ans = Math.min(ans, f(amount - c[i]));
+            } else {
+                break;
+            }
+        }
+
+        if(ans == Integer.MAX_VALUE) {
+            return dp[amount] = Integer.MAX_VALUE;
+        }
+        return dp[amount] = 1 + ans;
+    }
+
+    public int numSquares(int n) {
+        c = new int[100];
+        int j = 0;
+        for(int i = 1; i*i <= 10000; i++){
+            c[j] = i*i;
+            j++;
+        }
+        dp = new int [10005];
+        Arrays.fill(dp, -1);
+        int ans = f(n);
+        return ans;
+    }
+}
